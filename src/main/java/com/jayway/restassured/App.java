71c2bcf39bc7ffa7fa34159ec344cbcf65ac9dee
Johan Haleby <johan.haleby@gmail.com>
2012-06-07 08:25:17 +0200

    Created project

---
+++ src/main/java/com/jayway/restassured/App.java
@@ -0,0 +1,13 @@
+package com.jayway.restassured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
